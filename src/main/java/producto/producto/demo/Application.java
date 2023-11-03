8c51da0b4d7be239d6309add6dd360c49c553b8b
Jair-Delgado <jar.delgado@yavirac.edu.ec>
2023-11-03 10:18:51 -0500

    Arreglo de la clase main y puertos

--- src/main/java/producto/producto/demo/Application.java
+++ src/main/java/producto/producto/demo/Application.java
@@ -3,10 +3,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TareaApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(TareaApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
